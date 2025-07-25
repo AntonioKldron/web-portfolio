8e98e470f327aca5b8ffe00152e2315d80dd428c
Jose Cornelio <jcornelio@intelisis.com.mx>
2025-07-25 09:20:02 -0600

    hotfix

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -1,9 +1,16 @@
 import Slider from "react-slick";
-import TechCard from "./stackComponents/StackSection.jsx";
-
+import TechCard from "./stackComponents/StackSection.jsx"; 
 
 import { FaReact, FaGitAlt, FaDocker, FaAngular, FaDatabase } from "react-icons/fa";
-import { SiJavascript, SiPython, SiDotnet, SiPostgresql, SiDjango, SiVisualstudiocode, SiGithub, SiWamp, SiSupabase } from "react-icons/si";
+import {
+  SiJavascript,
+  SiPython,
+  SiDotnet,
+  SiPostgresql,
+  SiDjango,
+  SiGithub,
+  SiSupabase,
+} from "react-icons/si";
 import { TbSql } from "react-icons/tb";
 import { MdOutlineDataUsage } from "react-icons/md";
 
@@ -12,15 +19,16 @@ export default function StackSection() {
     dots: true,
     infinite: true,
     speed: 700,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    pauseOnHover: true,
+    autoplay: true, // <-- Asegúrate de que esto esté en 'true'
+    autoplaySpeed: 1000, // <-- Velocidad de cambio del carrusel (1 segundo)
+    arrows: false, // <-- Botones de navegación ocultos
+    pauseOnHover: true, // <-- Pausa el carrusel al pasar el ratón por encima
     responsive: [
       {
         breakpoint: 1024,
-        settings: { slidesToShow: 2 },
+        settings: { slidesToShow: 1 },
       },
       {
         breakpoint: 640,
@@ -61,7 +69,6 @@ export default function StackSection() {
       title: "Cloud e Infraestructura",
       items: [
         { icon: <FaDocker />, name: "Docker" },
-        { icon: <SiWamp />, name: "WAMP" },
         { icon: <SiSupabase />, name: "Supabase (BaaS)" },
       ],
     },
@@ -70,30 +77,49 @@ export default function StackSection() {
       items: [
         { icon: <FaGitAlt />, name: "Git" },
         { icon: <SiGithub />, name: "GitHub" },
-        { icon: <SiVisualstudiocode />, name: "VS Code" },
         { icon: <FaDatabase />, name: "SSMS" },
       ],
     },
   ];
 
   return (
-    <section className="px-6 py-20 text-white max-w-7xl mx-auto">
-      <h2 className="text-4xl font-bold text-center text-blue-400 mb-12">
+    <section className="px-6 py-20 text-white mx-auto min-h-screen bg-slate-900">
+      <h2 className="text-4xl font-bold text-center text-slate-400 mb-12">
         Stack Tecnológico
       </h2>
 
-      {categorias.map((categoria, idx) => (
-        <div key={idx} className="mb-16">
-          <h3 className="text-2xl font-semibold text-yellow-400 mb-4">
-            {categoria.title}
-          </h3>
-          <Slider {...settings}>
-            {categoria.items.map((tech, i) => (
-              <TechCard key={i} icon={tech.icon} name={tech.name} />
-            ))}
-          </Slider>
-        </div>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-25">
+        {categorias.slice(0, 3).map((categoria, idx) => (
+          <div key={idx} className="mb-4 md:mb-0">
+            <h3 className="text-2xl font-semibold text-stone-100 mb-4 text-center">
+              {categoria.title}
+            </h3>
+            <Slider {...settings}>
+              {categoria.items.map((tech, i) => (
+                <div key={i} className="flex justify-center items-center h-full">
+                  <TechCard icon={tech.icon} name={tech.name} />
+                </div>
+              ))}
+            </Slider>
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-8 max-w-4xl mx-auto">
+        {categorias.slice(3, 5).map((categoria, idx) => (
+          <div key={idx} className="mb-4 md:mb-0">
+            <h3 className="text-2xl font-semibold text-stone-100 mb-4 text-center">
+              {categoria.title}
+            </h3>
+            <Slider {...settings}>
+              {categoria.items.map((tech, i) => (
+                <div key={i} className="flex justify-center items-center h-full">
+                  <TechCard icon={tech.icon} name={tech.name} />
+                </div>
+              ))}
+            </Slider>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
--- src/components/stackComponents/StackSection.jsx
+++ src/components/stackComponents/StackSection.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export default function TechCard({ icon, name }) {
   return (
-    <div className="bg-[#1f2937] p-6 rounded-2xl shadow-md mx-3 h-40 w-60 flex flex-col items-center justify-center hover:scale-105 transform transition duration-300 ease-in-out">
-      <div className="text-4xl text-yellow-400 mb-3">{icon}</div>
+    <div className="bg-[#1f2937] p-6 rounded-2xl shadow-md mx-auto h-40 w-60 flex flex-col items-center justify-center hover:scale-125 transform transition duration-300 ease-in-out">
+      <div className="text-4xl text-slate-400 mb-3">{icon}</div>
       <h3 className="text-white text-lg font-semibold text-center">{name}</h3>
     </div>
   );
